6bfb725fe1438faaade635c07e80e159de553cde
“djrabi007” <rabi007007@gmail.com>
2020-02-24 01:17:29 +0530

    few check

--- UserAuthentication-ServiceRabi/src/main/java/com/stackroute/keepnote/UserAuthenticationServiceApplication.java
+++ UserAuthentication-ServiceRabi/src/main/java/com/stackroute/keepnote/UserAuthenticationServiceApplication.java
@@ -1,16 +1,18 @@
 package com.stackroute.keepnote;
 
 import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
 //@EnableFeignClients
+//@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class UserAuthenticationServiceApplication {
 	public static void main(String[] args) {
-		//Test more
 		SpringApplication.run(UserAuthenticationServiceApplication.class, args);
 		
 	}
